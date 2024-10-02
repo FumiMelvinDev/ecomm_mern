b294a7f25e5c62091239f545afa73bc55a5ada18
fumimelvindev <fumimelvindev@gmail.com>
2024-10-02 20:35:22 +0200

    Backend: connect mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+const colors = require("colors");
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`.cyan.underline);
 });
